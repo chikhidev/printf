b97aa9510a1059e118de21f75289cde50ace6532
gouzidev <gouzi.dev@gmail.com>
2023-04-23 17:11:10 -0700

    fixed print_str

--- functions0.c
+++ functions0.c
@@ -9,10 +9,18 @@
 unsigned int print_str(char *s)
 {
 unsigned int i = 1;
-while (s[i] != '\0')
-{
-	_putchar(s[i]);
-	i++;
+if (s == NULL) {
+_putchar('(');
+_putchar('n');
+_putchar('u');
+_putchar('l');
+_putchar('l');
+_putchar(')');
+return (6);
+}
+while (s[i]) {
+_putchar(s[i]);
+i++;
 }
 return (i);
 }
@@ -23,18 +31,16 @@ return (i);
  * Return: number of bytes written
  */
 
-unsigned int print_int(int n)
-{
+unsigned int print_int(int n) {
 unsigned int i = 1;
-if (n < 0)
-{
+
+if (n < 0) {
 i++;
 _putchar('-');
 n = -n;
 }
 
-if (n / 10 != 0)
-{
+if (n / 10 != 0) {
 i++;
 print_int(n / 10);
 }
@@ -48,8 +54,7 @@ return (i);
  * Return: number of bytes written
  */
 
-void print_bin(int n)
-{
+void print_bin(int n) {
 n < 0 ? n = -n : n;
 if (n / 2 != 0)
 print_bin(n / 2);
@@ -63,8 +68,7 @@ _putchar(n % 2 + 48);
  * Return: number of bytes written
  */
 
-void print_oct(int n)
-{
+void print_oct(int n) {
 n < 0 ? n = -n : n;
 if (n / 8 != 0)
 print_oct(n / 8);
@@ -78,11 +82,9 @@ _putchar(n % 8 + 48);
  * Return: number of bytes written
  */
 
-void print_hex(int n)
-{
+void print_hex(int n) {
 n < 0 ? n = -n : n;
-if (n / 16 != 0)
-{
+if (n / 16 != 0) {
 print_hex(n / 16);
 }
 (n % 16 < 10) ? _putchar(n % 16 + 48) : _putchar((n % 16 - 10) + 97);
