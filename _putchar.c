5747d05327230f58d615727ebf048ccfdd1fa8f5
gouzidev <gouzi.dev@gmail.com>
2023-04-24 07:31:40 -0700

    fixed betty for task 0

--- _putchar.c
+++ _putchar.c
@@ -9,11 +9,13 @@
 */
 int _putchar(char c)
 {
-    if (c == '\0') {
-        c = ' ';
-    }
-    if (write(1, &c, 1) == -1) {
-        return (-1);
-    }
-    return (1);
+	if (c == '\0')
+	{
+	c = ' ';
+	}
+	if (write(1, &c, 1) == -1)
+	{
+		return (-1);
+	}
+	return (1);
 }
