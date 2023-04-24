5b9f43bb32c746270c86df6de7b1c7a72e26904e
gouzidev <gouzi.dev@gmail.com>
2023-04-24 13:36:48 -0700

    idk

--- _printf.c
+++ _printf.c
@@ -9,23 +9,44 @@
  */
 int helper(char c, va_list args, int count)
 {
-switch (c)
-{
-case 'c':
-count += _putchar(char);
-break;
-case 's':
-count += print_str(va_arg(args, char*));
-break;
-case '%':
-count += _putchar('%');
-break;
-default:
-count += _putchar('%');
-count += _putchar(c);
-break;
-}
-return (count);
+
+	switch (c)
+	{
+	case 'c':
+		count += _putchar((char)va_arg(args, int));
+		break;
+	case 's':
+	case 'S':
+		count += _puts(va_arg(args, char*));
+		break;
+	case '%':
+		count += _putchar('%');
+		break;
+	case 'd':
+	case 'i':
+		count += print_int(va_arg(args, int));
+		break;
+	case 'u':
+		count += print_unsd(va_arg(args, unsigned int));
+		break;
+	case 'b':
+	case 'o':
+	case 'x':
+	case 'X':
+		count += dec_to_base(va_arg(args, unsigned int), c);
+		break;
+	case 'p':
+		count += print_pointer(va_arg(args, void *));
+		break;
+	case 'r':
+		count += _rev_str(va_arg(args, void *));
+		break;
+	default:
+		count += _putchar('%');
+		count += _putchar(c);
+		break;
+	}
+	return (count);
 }
 
 /**
@@ -36,33 +57,33 @@ return (count);
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	va_list args;
+	int count = 0;
 
-va_start(args, format);
+	va_start(args, format);
 
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
 
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
-while (*format && format)
-{
-if (*format == '%')
-{
-format++;
-count = helper(*format, args, count);
-if (count == -1)
-return (-1);
-}
-else
-{
-count += _putchar(*format);
-}
+	while (*format && format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count = helper(*format, args, count);
+			if (count == -1)
+				return (-1);
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
 
-format++;
-}
-va_end(args);
-return (count);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
