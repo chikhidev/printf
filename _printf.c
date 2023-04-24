c89417a8ab17ceaa81882e8c5e715f03e220b640
gouzidev <gouzi.dev@gmail.com>
2023-04-23 17:24:25 -0700

    fixing stuff

--- _printf.c
+++ _printf.c
@@ -1,80 +1,91 @@
 #include "main.h"
 
 /**
- * switcher- process a single conversion specifier in the printf format string
- * @check_case: the conversion specifier character
- * @vl: the argument list to extract the value to print from
- * @counter: the current character count
- * Return: the updated character count
+ * print_str - write a single char
+ * @s: str to write
+ * Return: number of bytes written
  */
 
-int switcher(char check_case, va_list vl, int counter)
+unsigned int print_str(char *s)
 {
-switch (check_case)
-{
-case 'c':
-_putchar(va_arg(vl, int));
-counter++;
-break;
-case 's':
-counter += print_str(va_arg(vl, char *));
-break;
-case 'i':
-case 'd':
-counter += print_int(va_arg(vl, int));
-break;
-case 'b':
-print_bin(va_arg(vl, int));
-break;
-case 'u':
-print_int(va_arg(vl, int));
-break;
-case 'o':
-print_int(va_arg(vl, int));
-break;
-case 'x':
-print_hex(va_arg(vl, int));
-break;
-case 'X':
-print_HEX(va_arg(vl, int));
-break;
-case '%':
-_putchar('%');
-break;
-default:
-_putchar(check_case);
-break;
+unsigned int i = 0;
+if (s == NULL) {
+write(1, "(null)", 6);
+return (6);
 }
-return (counter);
+if (!s)
+{
+write(1, "(null)", 6);
+return (6);
+}
+while (s[i]) {
+_putchar(s[i]);
+i++;
+}
+return (i);
 }
 
 /**
- * _printf - print a formatted string to standart output
- * @format: the printf format string to use
- * Return: total number of characters printed
+ * print_int - print a number
+ * @n: number to write
+ * Return: number of bytes written
  */
 
-int _printf(const char *format, ...)
-{
+unsigned int print_int(int n) {
+unsigned int i = 1;
 
-int i = 0;
-int counter = 0;
-va_list vl;
-va_start(vl, format);
-
-while (format[i])
-{
-if (format[i] == '%')
-{
+if (n < 0) {
 i++;
-counter = switcher(format[i], vl, counter);
-}
-else
-_putchar(format[i]);
-counter++;
-i++;
+_putchar('-');
+n = -n;
 }
 
-va_end(vl);
-return (counter);
+if (n / 10 != 0) {
+i++;
+print_int(n / 10);
+}
+_putchar(n % 10 + 48);
+return (i);
+}
+
+/**
+ * print_bin - print a number
+ * @n: number to write
+ * Return: number of bytes written
+ */
+
+void print_bin(int n) {
+n < 0 ? n = -n : n;
+if (n / 2 != 0)
+print_bin(n / 2);
+
+_putchar(n % 2 + 48);
+}
+
+/**
+ * print_oct - print a number
+ * @n: number to write
+ * Return: number of bytes written
+ */
+
+void print_oct(int n) {
+n < 0 ? n = -n : n;
+if (n / 8 != 0)
+print_oct(n / 8);
+
+_putchar(n % 8 + 48);
+}
+
+/**
+ * print_hex - print a number
+ * @n: number to write
+ * Return: number of bytes written
+ */
+
+void print_hex(int n) {
+n < 0 ? n = -n : n;
+if (n / 16 != 0) {
+print_hex(n / 16);
+}
+(n % 16 < 10) ? _putchar(n % 16 + 48) : _putchar((n % 16 - 10) + 97);
 }
